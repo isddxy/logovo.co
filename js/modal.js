6dfc270c3eeda78e1b18327e08a9de03f400db78
isddxy <milyaev.dev@gmail.com>
2020-10-24 20:35:35 +0300

    added modals for button to sale

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,23 @@
-$('.button').click(function(){
-    var buttonId = $(this).attr('id');
-    $('#modal-container').removeAttr('class').addClass(buttonId);
-    $('body').addClass('modal-active');
-  })
-  
-  $('.close-bg').click(function(){
-    $('#modal-container').addClass('out');
-    $('body').removeClass('modal-active');
-  });
+$(".button").click(function () {
+  var buttonId = $(this).attr("id");
+  $("#modal-container").removeAttr("class").addClass(buttonId);
+  $("body").addClass("modal-active");
+});
 
+$(".close-bg").click(function () {
+  $("#modal-container").addClass("out");
+  $("body").removeClass("modal-active");
+});
+
+
+
+$(".button2").click(function () {
+  var buttonId = $(this).attr("id");
+  $("#modal-container2").removeAttr("class").addClass(buttonId);
+  $("body").addClass("modal-active");
+});
+
+$(".close-bg").click(function () {
+  $("#modal-container2").addClass("out");
+  $("body").removeClass("modal-active");
+});
